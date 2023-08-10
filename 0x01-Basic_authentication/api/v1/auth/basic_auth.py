3d156b2ce525f33e5a8c9ad49b1bc01564490c0c
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-08-10 19:28:41 +0000

    extract user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,3 +31,13 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        if decoded_base64_authorization_header is None or not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        
+        email, password = decoded_base64_authorization_header.split(":", 1)
+        return email, password

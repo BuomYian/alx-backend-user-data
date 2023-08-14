3ef402473153e821bc38204ba5860e3710d09e03
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-08-14 19:13:04 +0300

    session auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,7 +7,9 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -16,11 +18,11 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None  # Initializing auth a None
 
 if os.getenv("AUTH_TYPE") == "auth":
-    from api.v1.auth.auth import Auth
     auth = Auth()
 elif os.getenv("AUTH_TYPE") == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif os.getenv("AUTH_TYPE") == "session_auth":
+    auth = SessionAuth()
 
 
 @app.before_request

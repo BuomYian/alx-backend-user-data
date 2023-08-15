9a42a2e8cccc3f049dffed97621d72fd302f8c04
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-08-15 21:28:22 +0300

    fixed

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 """
-Initialize the view blueprint
+Initialize the views blueprint
 """
-from api.v1.views.session_auth import *
-from api.v1.views.users import *
+
 from api.v1.views.index import *
+from api.v1.views.users import *
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
-User.load_from_file()
+def import_session_auth_view():
+    from api.v1.views.session_auth import create_session_auth_view
+    app = create_session_auth_view()
+    app_views.register_blueprint(app)
+
+
+# import and register the session_auth view
+import_session_auth_view()

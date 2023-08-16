4f39e3be35ca83ce5b6e8463c4d7c192854e88c7
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-08-16 09:16:14 +0000

    remove unused function

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -6,7 +6,9 @@ This module defines the User model for the 'users' table.
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
+
 
 class User(Base):
     """
@@ -20,6 +22,3 @@ class User(Base):
     hashed_password = Column(String(length=250), nullable=False)
     session_id = Column(String(length=250), nullable=True)
     reset_token = Column(String(length=250), nullable=True)
-
-    def __repr__(self):
-        return f"<User(id={self.id}, email='{self.email}')>"

1bcac067fbab7cf60e5ad8acfacd179055273470
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-08-17 09:08:55 +0000

    POST method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,8 +4,10 @@ Simple Flask app with user authentication features.
 """
 
 from flask import Flask, jsonify
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
@@ -15,6 +17,20 @@ def welcome() -> str:
         - The home page's payload.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_user() -> str:
+    """Define a route for POST requests to /users
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

d955bc15fda12e9021504eb488df7f16fe050434
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-08-17 08:43:22 +0000

    updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -34,6 +34,8 @@ class Auth:
         """
         try:
             self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            return self._db.add_user(email, _hash_password(password))
-        raise ValueError(f"User ${email} already exists")
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
